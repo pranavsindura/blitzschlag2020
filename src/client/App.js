bc84abac4db2a517d12b9b532c79de93f66d4879
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-18 14:19:17 +0530

    Added Home

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import Home from './Home.js'
 
 export default class App extends Component {
-  state = { username: null };
+	state = {};
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+	componentDidMount() {
+		fetch('/api/getUsername')
+			.then((res) => res.json())
+			.then((user) => this.setState({ username: user.username }));
+	}
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Home/>
+			</div>
+		);
+	}
 }
---
+++ src/client/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+	state = {};
+
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
