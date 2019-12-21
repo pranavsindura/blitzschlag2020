65c7625a6717d7622e61a874c54ec13d0143537b
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-21 18:47:22 +0530

    bug fix

--- src/client/Trackslidermob.js
+++ src/client/Trackslidermob.js
@@ -3,7 +3,7 @@ import Coverflow from 'react-coverflow';
 import { StyleRoot } from 'radium';
 import disableScroll from 'disable-scroll';
 import { Link } from 'react-router-dom';
-import './TracksliderMob.css';
+import './Trackslidermob.css';
 
 export default class TrackSliderMob extends Component {
 	state = {};
