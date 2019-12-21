0d344a7961387b891e32d80358598d262724158f
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-21 19:22:44 +0530

    down arrow

--- src/client/App.js
+++ src/client/App.js
@@ -38,6 +38,46 @@ export default class App extends Component {
 					>
 						<g id="chevron-left">
 							<polygon
+								style={{ stroke: '#D80035', strokeWidth: '20px', fill: 'white' }}
+								points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153 		"
+							/>
+						</g>
+					</svg>
+				</div>
+				<div className="downbutton b1">
+					<svg
+						version="1.1"
+						id="Capa_1"
+						xmlns="http://www.w3.org/2000/svg"
+						x="0px"
+						y="0px"
+						width="30px"
+						height="30px"
+						viewBox="0 0 306 306"
+						style={{ enableBackground: 'new 0 0 306 306' }}
+					>
+						<g id="chevron-left">
+							<polygon
+								style={{ stroke: '#D80035', strokeWidth: '10px', fill: 'white' }}
+								points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153 		"
+							/>
+						</g>
+					</svg>
+				</div>
+				<div className="downbutton b2">
+					<svg
+						version="1.1"
+						id="Capa_1"
+						xmlns="http://www.w3.org/2000/svg"
+						x="0px"
+						y="0px"
+						width="30px"
+						height="30px"
+						viewBox="0 0 306 306"
+						style={{ enableBackground: 'new 0 0 306 306' }}
+					>
+						<g id="chevron-left">
+							<polygon
 								style={{ stroke: '#D80035', strokeWidth: '10px', fill: 'white' }}
 								points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153 		"
 							/>
