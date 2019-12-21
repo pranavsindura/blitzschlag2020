424763611e4f004f3e86b80cee5ec415f84da7ab
Satya Dhwaj <dhwajsatya@gmail.com>
2019-12-21 23:54:00 +0530

    Event Carousel Replaced

--- src/client/Trackslider.js
+++ src/client/Trackslider.js
@@ -36,8 +36,6 @@ export default  class TrackSlider extends Component {
         enableHeading={false}
       >
         <div
-        // onClick={() => fn()}
-        // onKeyDown={() => fn()}
         >
           <img
             className="img-responsive"
--- src/client/event.js
+++ src/client/event.js
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
 import "./Event.css";
-import TrackSlider from "./Trackslider";
-import TrackSliderMob from "./Trackslidermob.js";
+import ControlledCarousel from "./EventCar.js"
 export default class Event extends Component {
-  rendercar() {
-    if (window.innerWidth > 700) {
-      return <TrackSlider></TrackSlider>;
-    } else {
-      return <TrackSliderMob />;
-    }
-  }
   render() {
     return (
       <div>
         <h1>Events</h1>
-        <br></br>
-        <br></br>
-        {this.rendercar()}
+         <ControlledCarousel />
       </div>
     );
   }
