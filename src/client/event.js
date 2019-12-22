685d2ea8c71a2d02c473dc4128d1788a1d2cd53e
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-22 15:42:57 +0530

    event bug fix

--- src/client/event.js
+++ src/client/event.js
@@ -2,24 +2,14 @@ import React, { Component } from 'react';
 import './Event.css';
 import ControlledCarousel from './EventCar.js';
 import ReactFullpage from '@fullpage/react-fullpage';
+import { Row, Col } from 'react-bootstrap';
 export default class Event extends Component {
 	render() {
 		return (
-				<ReactFullpage
-					scrollingSpeed={1000}
-					verticalCentered={true}
-					touchSensitivity={15}
-					render={({ state, fullpageApi }) => {
-						return (
-							<ReactFullpage.Wrapper>
-								<div className="section">
-									<h1>Events</h1>
-									<ControlledCarousel />
-								</div>
-							</ReactFullpage.Wrapper>
-						);
-					}}
-				/>
+			<div>
+					<h1 style={{position: 'absolute', zIndex:'1', left:'0', right:'0', backgroundColor:"black", padding:'10px'}}>Events</h1>
+				<ControlledCarousel />
+			</div>
 		);
 	}
 }
