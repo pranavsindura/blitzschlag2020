778d37307868cfe1d9a5fb5d18611d8d2ad5df6a
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-23 23:19:24 +0530

    url fix

--- src/server/index.js
+++ src/server/index.js
@@ -4,9 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('*',(req,res)=>{
-    console.log(__dirname);
-    res.sendFile(path.resolve('./dist/index.html')) 
-})
+app.get('*', (req, res) => {
+	res.sendFile('./dist/index.html');
+});
 console.log(process.env.PORT);
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
