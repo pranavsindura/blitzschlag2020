45fe5bc085010a9120d7840358b5ff98eccb0638
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-23 23:30:58 +0530

    url fix

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.static('dist'));
 app.get('*',(req,res)=>{
     console.log(__dirname);
-    res.sendFile(path.resolve('./dist/index.html')) 
+    // res.sendFile(path.resolve('./dist/index.html')) 
+    res.redirect('https://blitz20.herokuapp.com/');
 })
 console.log(process.env.PORT);
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
