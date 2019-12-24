1b123f357258b00154788da6a0a9585d23b6c8c2
Pranav Sindura <pranav.sindura@gmail.com>
2019-12-24 15:41:43 +0530

    Redux added

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import reducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logAction = (store)=>{
+    return (next) => {
+        return (action)=> {
+            console.log('Dispatching: '+JSON.stringify(action));
+            const result = next(action);
+            console.log('New State: ' + JSON.stringify(store.getState()))
+            return result;
+        }
+    }
+}
+
+let store = createStore(reducer, applyMiddleware(logAction));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
