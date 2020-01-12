5c0e7843f40a892131bcdffd9050eea8f51704bf
Dhairya-patel <dhairya.patel0@gmail.com>
2020-01-13 01:24:09 +0530

    Event registration mailer

--- src/server/mailer.js
+++ src/server/mailer.js
@@ -22,7 +22,29 @@ async function main(obj) {
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
 
+async function eventMail(user, teamID, eventName) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'butterKiDukan@gmail.com',
+            pass: 'shalu0907'
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Dhairya" <butterKiDukan@gmail.com>',
+        to: user.email,
+        subject: `Event Registration Confirmed for ${eventName}`,
+        text: "Its a text",
+        html: `<b>You Are Registered with team id ${teamID} </b>`
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
 // main().catch(console.error);
 module.exports = {
-    main
+    main,
+    eventMail
 }
