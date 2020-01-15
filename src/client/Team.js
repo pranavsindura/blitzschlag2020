b188533886c789bf5af613454e15a679b1518ea2
Satya Dhwaj <dhwajsatya@gmail.com>
2020-01-15 16:40:22 +0530

    bug fix

--- src/client/Team.js
+++ src/client/Team.js
@@ -77,15 +77,15 @@ export default class Team extends Component {
           render={({ state, fullpageApi }) => {
             return (
               <ReactFullpage.Wrapper>
-                <div className="section" style={{background:'#000000'}}>
-                <a href="http://www.blitzschlag.co.in/">
-                <div style={{padding:'0px',margin:'0px'}}>
-                  
-                      <img 
-                      style={{height:'auto',width:'100px'}}
-                      src="src/shared/img/blitzlogo.png"></img>
+                <div className="section" style={{ background: "#000000" }}>
+                  <a href="http://www.blitzschlag.co.in/">
+                    <div style={{ padding: "0px", margin: "0px" }}>
+                      <img
+                        style={{ height: "auto", width: "100px" }}
+                        src="src/shared/img/blitzlogo.png"
+                      ></img>
                     </div>
-                    </a>
+                  </a>
                   <div className="container team-cont">
                     <h2 className="team-head singlecont">Team Blitzschlag</h2>
                     <br></br>
@@ -724,6 +724,11 @@ export default class Team extends Component {
                         </div>
                       </div>
                     </div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
                   </div>
                 </div>
               </ReactFullpage.Wrapper>
