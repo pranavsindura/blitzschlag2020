0e227898a156facf931ba1c7e162767c0b59c73c
Dhairya-patel <dhairya.patel0@gmail.com>
2020-01-18 17:14:03 +0530

    mailer changed with content

--- src/server/mailer.js
+++ src/server/mailer.js
@@ -15,7 +15,7 @@ async function main(obj) {
         to: obj.email,
         subject: `Registration Confirmed of ${obj.firstName}`,
         text: "Its a text",
-        html: `<b>You Are Registered with id ${obj.blitzID} </b>`
+        html: `<p>Hello <b>${obj.firstName}</b><p><br><br><p>Greetings From <b>Blitzschlag'20</b></p><br><br><p>Your account has been successfully created. Please find your account details below</p><br><br><p>You Are Registered with id blitz20@<b>${obj.blitzID}</b></p><br><br><p>Name: ${obj.firstName}</p><br><p>Email: ${obj.email}</p><br><p>Registration ID: blitz20@${obj.blitzID}</p><br><br><p>Please don't share your registration ID and password with anyone. If you are a student outside MNIT Jaipur, IIIT Kota, NIT UK please complete your payment</p><br><br>For payment and accomodation contact us: <br><br><p>Ansh Khandelwal</p><br><p>8518066443</p><br><br><p>Divyaman</p><br><p>8963052212</p><br><br><br><p>**This is auto generated mail, please do not reply**</p>`
     });
 
     // console.log("Message sent: %s", info.messageId);
@@ -36,7 +36,7 @@ async function eventMail(user, teamID, eventName) {
         to: user.email,
         subject: `Event Registration Confirmed for ${eventName}`,
         text: "Its a text",
-        html: `<b>You Are Registered with team id ${teamID} </b>`
+        html: `<p>Hello <b>${user.firstName}</b><p><br><br><p>Greetings From <b>Blitzschlag'20</b></p><br><br><p>Your are Registered for - ${eventName}</p><br><br><p>Your team ID is - <b>${teamID}</b> </p><br><br><p>**This is auto generated mail, please do not reply**</p>`
     });
 
     // console.log("Message sent: %s", info.messageId);
